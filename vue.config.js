6172d8889fb06537e509e9c628c5ae8a5e7514d3
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2022-12-09 15:17:01 +0900

    Refactor

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // mode: 'production'
-//   devServer: {
-//     port: 8888,
-// },
 })
